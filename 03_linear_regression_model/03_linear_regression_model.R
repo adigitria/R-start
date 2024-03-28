6b46538c9d8951d7b90c726474fe4b06a9ffd7f3
Svetlana Bogomaz <s.m.bogomaz@gmail.com>
2024-03-28 01:55:00 +0100

    Add 03_linear_regression_model

---
+++ 03_linear_regression_model/03_linear_regression_model.R
@@ -0,0 +1,23 @@
+
+# Task 3: Linear Regression Modeling
+
+# Load libraries
+library(tidyverse)
+library(broom)
+
+# Load dataset
+data <- read.csv("your_dataset.csv")
+
+# Fit linear model
+model <- lm(Outcome ~ Predictor1 + Predictor2, data = data)
+
+# Summary
+summary(model)
+
+# Tidy output
+tidy(model)
+
+# Plot regression line
+ggplot(data, aes(x = Predictor1, y = Outcome)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
