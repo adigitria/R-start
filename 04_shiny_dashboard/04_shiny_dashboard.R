a7278db60c91dba7013beab0264dfe4259f50d98
Svetlana Bogomaz <s.m.bogomaz@gmail.com>
2024-04-10 01:37:12 +0200

    Add 04_shiny_dashboard

---
+++ 04_shiny_dashboard/04_shiny_dashboard.R
@@ -0,0 +1,41 @@
+
+# Task 4: Shiny Dashboard
+
+library(shiny)
+library(tidyverse)
+
+# Sample data
+data <- read.csv("your_dataset.csv")
+
+# UI
+ui <- fluidPage(
+  titlePanel("Simple Shiny Dashboard"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("category", "Choose Category:", choices = unique(data$CategoryColumn))
+    ),
+    mainPanel(
+      plotOutput("plot"),
+      tableOutput("table")
+    )
+  )
+)
+
+# Server
+server <- function(input, output) {
+  filtered_data <- reactive({
+    data %>% filter(CategoryColumn == input$category)
+  })
+
+  output$plot <- renderPlot({
+    ggplot(filtered_data(), aes(x = VariableX, y = VariableY)) +
+      geom_point()
+  })
+
+  output$table <- renderTable({
+    filtered_data()
+  })
+}
+
+# Run the app
+shinyApp(ui = ui, server = server)
